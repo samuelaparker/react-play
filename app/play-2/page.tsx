eeb1ae36f801cf7e6a1b99186097cbdf14c04bd7
Samuel Parker <samuel.a.parker00@gmail.com>
2024-11-18 14:44:13 -0500

    add another test page. to do: expirement with shad cn components

---
+++ app/play-2/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <div className="container border-red-500 border-4 m-auto h-screen py-4">
+        <div className="flex justify-center flex-col items-center">
+          {Array.from({ length: 8 }, (_, index) => (
+            <div
+              key={index}
+              className="border-green-400 border-4 w-full flex flex-col items-center"
+            >
+              page
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
